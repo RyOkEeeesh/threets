84703fb761563f981e80a85fe37702cb135a220e
RyOkEeeesh <dafe00842202@outlook.jp>
2025-06-05 17:54:39 +0900

    commit

---
+++ pinpon/vite.config.ts
@@ -0,0 +1,13 @@
+import { defineConfig } from 'vite';
+
+export default defineConfig({
+  root: './', // プロジェクトのルート
+  server: {
+    open: true, // ブラウザで自動オープン
+    port: 3000, // 任意のポート番号
+  },
+  build: {
+    outDir: 'dist',
+    emptyOutDir: true,
+  },
+});
