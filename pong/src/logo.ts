65d9c04d0165da1e9973015fd471f82d72f881fe
RyOkEeeesh <dafe00842202@outlook.jp>
2025-06-11 13:29:20 +0900

    commit

--- pong/src/logo.ts
+++ pong/src/logo.ts
@@ -12,18 +12,22 @@ const app = new App({
   composer: true
 });
 
+
 const txt = new txtMesh()
 
 const text = await txt.loadFontText(
   'PONG',
-  /* geometry option */ {
-    depth: 0.1,
-  }, /* material option */ {
-    color: 0x7fba00,
-    emissive: 0xa6a6a7,
-    emissiveIntensity: 1, // 自発光の強さ
-    metalness: 0, // 金属っぽさ 0 : プラスチック　1 : 金属
-    roughness: 0 // 表面の粗さ 0: つるつる　1 : ざらざら
+  {
+    geometryOption: {
+      depth: 0.1,
+    },
+    materialOption: {
+      color: 0x7fba00,
+      emissive: 0xa6a6a7,
+      emissiveIntensity: 1, // 自発光の強さ
+      metalness: 0, // 金属っぽさ 0 : プラスチック　1 : 金属
+      roughness: 0 // 表面の粗さ 0: つるつる　1 : ざらざら
+    }
   }
 );
 
