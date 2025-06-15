4802ac3481ea3194469da5dbea415a0b4fa3b92e
RyOkEeeesh <dafe00842202@outlook.jp>
2025-06-15 19:30:18 +0900

    パドル反射後のエフェクト追加 ToDo ユーザ操作の追加 ポイント加算処理 点数表示ディスプレイの作成

--- pong/src/game.ts
+++ pong/src/game.ts
@@ -11,8 +11,20 @@ const ballSpeed = 20;
 let velocity = TC.vec3({x: 0.1, z: 0.1}).normalize().multiplyScalar(ballSpeed);
 game.ballVelocity = velocity;
 
+const keyState: Record<string, boolean> = {};
+
+window.addEventListener('keydown', (e) => {
+  keyState[e.code] = true;
+});
+
+window.addEventListener('keyup', (e) => {
+  keyState[e.code] = false;
+  });
+
 game.onBeforeRender(() => {
   game.reflectorGame();
+  // console.log(keyState)
+
 });
 
 game.start();
