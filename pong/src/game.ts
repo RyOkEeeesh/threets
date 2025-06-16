2f28a7bd90baa19e69510e1add1d73d0b391f015
RyOkEeeesh <dafe00842202@outlook.jp>
2025-06-16 18:24:14 +0900

    パドルの操作　コンピュータの追加
    AIの動きをもっとよくする
    得点加算

--- pong/src/game.ts
+++ pong/src/game.ts
@@ -7,24 +7,7 @@ const game = new Game({
 });
 
 
-const ballSpeed = 20;
-let velocity = TC.vec3({x: 0.1, z: 0.1}).normalize().multiplyScalar(ballSpeed);
+let velocity = TC.vec3({x: 0.1, z: 0.1}).normalize().multiplyScalar(game.ballSpeed);
 game.ballVelocity = velocity;
-
-const keyState: Record<string, boolean> = {};
-
-window.addEventListener('keydown', (e) => {
-  keyState[e.code] = true;
-});
-
-window.addEventListener('keyup', (e) => {
-  keyState[e.code] = false;
-  });
-
-game.onBeforeRender(() => {
-  game.reflectorGame();
-  // console.log(keyState)
-
-});
 
 game.start();
